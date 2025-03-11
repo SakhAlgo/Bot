2e3ab70b6adc29f09dffdf0886c750d68a77cce5
SakhAlgo <sakhalgo@gmail.com>
2025-03-11 19:58:18 +1100

    3

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state
 from aiogram.types import (CallbackQuery, InlineKeyboardButton,
                            InlineKeyboardMarkup, Message, PhotoSize)
-from config import Config, load_config
+# from config import Config, load_config
 
-config: Config = load_config()
+# config: Config = load_config()
 # BOT_TOKEN: str = config.tg_bot.token
 BOT_TOKEN = '7961876042:AAH7CsB5ZVJIxJVZ_3OrqWOHL62NhOAySVY'
 # Создаем объекты бота и диспетчера
